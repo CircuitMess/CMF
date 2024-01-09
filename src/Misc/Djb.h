6c4b3d7d6712b3701a67232d80e4bffe49dfb7b8
Fr0zen3k <jan.kresic@circuitmess.com>
2024-01-09 10:04:24 +0100

    Cleanup

--- src/Misc/Djb.h
+++ src/Misc/Djb.h
@@ -42,43 +42,42 @@ static inline constexpr uint32_t DJB_SH(uint32_t start, uint8_t character) noexc
 #define DJB_SH_31(a) DJB_SH(DJB_SH_30(a), (a)[30])
 #define DJB_SH_32(a) DJB_SH(DJB_SH_31(a), (a)[31])
 
-// initial check prevents too-large strings from compiling
-#define STRING_HASH(a) ( \
-    (void)(sizeof(int[(sizeof(a) > 33 ? -1 : 1)])), \
-    (sizeof(a) == 1) ? DJB_SEED : \
-    (sizeof(a) == 2) ? DJB_SH_1(a) : \
-    (sizeof(a) == 3) ? DJB_SH_2(a) : \
-    (sizeof(a) == 4) ? DJB_SH_3(a) : \
-    (sizeof(a) == 4) ? DJB_SH_3(a) : \
-    (sizeof(a) == 5) ? DJB_SH_4(a) : \
-    (sizeof(a) == 6) ? DJB_SH_5(a) : \
-    (sizeof(a) == 7) ? DJB_SH_6(a) : \
-    (sizeof(a) == 8) ? DJB_SH_7(a) : \
-    (sizeof(a) == 9) ? DJB_SH_8(a) : \
-    (sizeof(a) == 10) ? DJB_SH_9(a) : \
-    (sizeof(a) == 11) ? DJB_SH_10(a) : \
-    (sizeof(a) == 12) ? DJB_SH_11(a) : \
-    (sizeof(a) == 13) ? DJB_SH_12(a) : \
-    (sizeof(a) == 14) ? DJB_SH_13(a) : \
-    (sizeof(a) == 15) ? DJB_SH_14(a) : \
-    (sizeof(a) == 16) ? DJB_SH_15(a) : \
-    (sizeof(a) == 17) ? DJB_SH_16(a) : \
-    (sizeof(a) == 18) ? DJB_SH_17(a) : \
-    (sizeof(a) == 19) ? DJB_SH_18(a) : \
-    (sizeof(a) == 20) ? DJB_SH_19(a) : \
-    (sizeof(a) == 21) ? DJB_SH_20(a) : \
-    (sizeof(a) == 22) ? DJB_SH_21(a) : \
-    (sizeof(a) == 23) ? DJB_SH_22(a) : \
-    (sizeof(a) == 24) ? DJB_SH_23(a) : \
-    (sizeof(a) == 25) ? DJB_SH_24(a) : \
-    (sizeof(a) == 26) ? DJB_SH_25(a) : \
-    (sizeof(a) == 27) ? DJB_SH_26(a) : \
-    (sizeof(a) == 28) ? DJB_SH_27(a) : \
-    (sizeof(a) == 29) ? DJB_SH_28(a) : \
-    (sizeof(a) == 30) ? DJB_SH_29(a) : \
-    (sizeof(a) == 31) ? DJB_SH_30(a) : \
-    (sizeof(a) == 32) ? DJB_SH_31(a) : \
-    (sizeof(a) == 33) ? DJB_SH_32(a) : \
-    0)
+#define STRING_HASH(a) ( 										\
+    (void)(sizeof(int[(sizeof(a) > 33 ? -1 : 1)])), 			\
+    (sizeof(a) == 1) ? DJB_SEED : 								\
+    (sizeof(a) == 2) ? DJB_SH_1(a) : 							\
+    (sizeof(a) == 3) ? DJB_SH_2(a) : 							\
+    (sizeof(a) == 4) ? DJB_SH_3(a) : 							\
+    (sizeof(a) == 4) ? DJB_SH_3(a) : 							\
+    (sizeof(a) == 5) ? DJB_SH_4(a) : 							\
+    (sizeof(a) == 6) ? DJB_SH_5(a) : 							\
+    (sizeof(a) == 7) ? DJB_SH_6(a) : 							\
+    (sizeof(a) == 8) ? DJB_SH_7(a) : 							\
+    (sizeof(a) == 9) ? DJB_SH_8(a) : 							\
+    (sizeof(a) == 10) ? DJB_SH_9(a) : 							\
+    (sizeof(a) == 11) ? DJB_SH_10(a) : 							\
+    (sizeof(a) == 12) ? DJB_SH_11(a) : 							\
+    (sizeof(a) == 13) ? DJB_SH_12(a) : 							\
+    (sizeof(a) == 14) ? DJB_SH_13(a) : 							\
+    (sizeof(a) == 15) ? DJB_SH_14(a) : 							\
+    (sizeof(a) == 16) ? DJB_SH_15(a) : 							\
+    (sizeof(a) == 17) ? DJB_SH_16(a) : 							\
+    (sizeof(a) == 18) ? DJB_SH_17(a) : 							\
+    (sizeof(a) == 19) ? DJB_SH_18(a) : 							\
+    (sizeof(a) == 20) ? DJB_SH_19(a) : 							\
+    (sizeof(a) == 21) ? DJB_SH_20(a) : 							\
+    (sizeof(a) == 22) ? DJB_SH_21(a) : 							\
+    (sizeof(a) == 23) ? DJB_SH_22(a) : 							\
+    (sizeof(a) == 24) ? DJB_SH_23(a) : 							\
+    (sizeof(a) == 25) ? DJB_SH_24(a) : 							\
+    (sizeof(a) == 26) ? DJB_SH_25(a) : 							\
+    (sizeof(a) == 27) ? DJB_SH_26(a) : 							\
+    (sizeof(a) == 28) ? DJB_SH_27(a) : 							\
+    (sizeof(a) == 29) ? DJB_SH_28(a) : 							\
+    (sizeof(a) == 30) ? DJB_SH_29(a) : 							\
+    (sizeof(a) == 31) ? DJB_SH_30(a) : 							\
+    (sizeof(a) == 32) ? DJB_SH_31(a) : 							\
+    (sizeof(a) == 33) ? DJB_SH_32(a) : 							\
+    0)															\
 
 #endif //CMF_DJB_H
