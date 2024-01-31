9461c91ae3f524312ba941f777c876652b006f52
Fr0zen3k <jan.kresic@circuitmess.com>
2024-01-31 11:07:39 +0100

    Trace log optimization

--- src/Log/Log.h
+++ src/Log/Log.h
@@ -5,6 +5,6 @@
 #include <string>
 #include <esp_log.h>
 
-#define TRACE_LOG(format, ...) printf(std::string("%s: %s (%d) ").append(format).append("\n").c_str(), __FILE__, __func__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
+#define TRACE_LOG(format, ...) printf(std::string("%s: %s (%d) ").append(format).append("\n").c_str(), __FILE__, __FUNCTION__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
 
 #endif //CMF_LOG_H
