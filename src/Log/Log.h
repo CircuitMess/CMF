d338c46a825e397724616660b2e972ee6d804c96
Fr0zen3k <jan.kresic@circuitmess.com>
2024-02-13 15:24:41 +0100

    CMF log

--- src/Log/Log.h
+++ src/Log/Log.h
@@ -6,6 +6,36 @@
 #include <esp_log.h>
 #include <source_location>
 
+enum class LogLevel : uint8_t {
+	None = (uint8_t) ESP_LOG_NONE,
+	Error = (uint8_t) ESP_LOG_ERROR,
+	Warning = (uint8_t) ESP_LOG_WARN,
+	Info = (uint8_t) ESP_LOG_INFO,
+	Debug = (uint8_t) ESP_LOG_DEBUG,
+	Verbose = (uint8_t) ESP_LOG_VERBOSE
+};
+
+struct LogType {
+	const std::string Tag;
+};
+
+#define DEFINE_LOG(name) LogType name = {#name};
+DEFINE_LOG(LogTemp)
+DEFINE_LOG(LogCMF)
+
 #define TRACE_LOG(format, ...) printf(std::string("%s(%d:%d) '%s' ").append(format).append("\n").c_str(), std::source_location::current().file_name(),std::source_location::current().line(), std::source_location::current().column(), std::source_location::current().function_name() __VA_OPT__(,) __VA_ARGS__)
+
+#define CMF_LOG(log, level, format, ...)																	\
+	if(level == LogLevel::Error){																			\
+		ESP_LOGE(log.Tag.c_str(), format __VA_OPT__(,) __VA_ARGS__);										\
+	}else if(level == LogLevel::Warning){																	\
+		ESP_LOGW(log.Tag.c_str(), format __VA_OPT__(,) __VA_ARGS__);										\
+	}else if(level == LogLevel::Info){																		\
+		ESP_LOGI(log.Tag.c_str(), format __VA_OPT__(,) __VA_ARGS__);										\
+	}else if(level == LogLevel::Debug){																		\
+		ESP_LOGD(log.Tag.c_str(), format __VA_OPT__(,) __VA_ARGS__);										\
+	}else if(level == LogLevel::Verbose){																	\
+		ESP_LOGV(log.Tag.c_str(), format __VA_OPT__(,) __VA_ARGS__);										\
+	}else{}																									\
 
 #endif //CMF_LOG_H
