d18f224c9d9d833f20a9787dc247ae6c609573b2
Emil Gajšak <emil.gajsak@gmail.com>
2025-01-30 14:59:51 +0100

    PWM output, Motors abstraction (#11)
    * OutputPWM
    
    * Motors, MotorsEaser implementation
    
    * OutputMCPWM
    
    * Servos implementation
    
    * Include and guard cleanup, template and cast cleanup
    
    ---------
    
    Co-authored-by: Fr0zen3k <jan.kresic@circuitmess.com>

---
+++ src/Drivers/Output/OutputMCPWM.h
@@ -0,0 +1,57 @@
+#ifndef CMF_OUTPUTMCPWM_H
+#define CMF_OUTPUTMCPWM_H
+
+#include "Drivers/Interface/OutputDriver.h"
+#include "driver/mcpwm_prelude.h"
+#include <hal/gpio_types.h>
+
+/**
+ * OutputPinDef.port represents an independently controlled timer within an MCPWM group,
+ * while OutputPWMPinDef.pin is its attached GPIO
+ */
+struct OutputMCPWMPinDef : OutputPinDef {
+	gpio_num_t pin;
+
+	uint32_t TimerResolution;  // Number of timer ticks per second
+	uint32_t PeriodLength;    // Duration of a period in ticks
+	uint32_t MinPulseWidth;  // Minimum pulse width in TimerResolution ticks, [0 - PeriodLength]
+	uint32_t MaxPulseWidth;  // Maximum pulse width in TimerResolution ticks, [0 - PeriodLength]
+
+};
+
+
+class OutputMCPWM : public OutputDriver<OutputMCPWMPinDef> {
+	GENERATED_BODY(OutputMCPWM, OutputDriver)
+public:
+	OutputMCPWM() = default;
+
+	explicit OutputMCPWM(const std::vector<OutputMCPWMPinDef>& outputs);
+
+	void enable(int port);
+
+	void disable(int port);
+
+private:
+	void performWrite(int port, float value) noexcept override;
+
+	void performRegister(OutputMCPWMPinDef output) noexcept override;
+
+	void performDeregister(OutputMCPWMPinDef output) noexcept override;
+
+	static constexpr uint8_t PortsNum = SOC_MCPWM_GROUPS * SOC_MCPWM_TIMERS_PER_GROUP;
+
+	struct PortState {
+		uint32_t MinPulseWidth;  // Minimum pulse width in TimerResolution ticks, [0 - PeriodLength]
+		uint32_t MaxPulseWidth;  // Maximum pulse width in TimerResolution ticks, [0 - PeriodLength]
+
+		mcpwm_timer_handle_t timerHandle;
+		mcpwm_oper_handle_t operatorHandle;
+		mcpwm_cmpr_handle_t compHandle;
+		mcpwm_gen_handle_t genHandle;
+
+		std::atomic_bool enabled = false;
+	} states[PortsNum];
+
+};
+
+#endif //CMF_OUTPUTMCPWM_H
---
+++ src/Services/Motors/MotorsEaser.h
@@ -0,0 +1,34 @@
+#ifndef CMF_MOTORSEASER_H
+#define CMF_MOTORSEASER_H
+
+#include "Object/Object.h"
+
+/**
+ * Interface for easing the ramp-up over time of DC motors.
+ */
+class MotorsEaser : public Object {
+	GENERATED_BODY(MotorsEaser, Object)
+public:
+	virtual float easeValue(float dt, float currentValue, float targetValue){
+		return targetValue;
+	}
+};
+
+class LinearEaser : public MotorsEaser {
+	GENERATED_BODY(LinearEaser, MotorsEaser)
+public:
+	explicit LinearEaser(const float easeStrength = DefaultEaseStrength) : Super(), easeStrength(easeStrength){}
+
+	float easeValue(float dt, float currentValue, float targetValue) override{
+		float direction = (currentValue < targetValue) ? 1 : -1;
+
+		return currentValue + direction * easeStrength * dt;
+	}
+
+private:
+	const float easeStrength;
+
+	static constexpr float DefaultEaseStrength = 200; //value change per second, easing speed
+};
+
+#endif //CMF_MOTORSEASER_H
