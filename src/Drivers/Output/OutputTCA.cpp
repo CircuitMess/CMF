7feedda6ac43715ae4ce24f867784b4f590de412
Emil Gajšak <emil.gajsak@gmail.com>
2025-08-29 15:56:04 +0200

    Merge pull request #22 from CircuitMess/TCA9555-CMF-9
    TCA9555

---
+++ src/Drivers/Output/OutputTCA.cpp
@@ -0,0 +1,17 @@
+#include "OutputTCA.h"
+
+OutputTCA::OutputTCA(const std::vector<OutputPinDef>& outputs, TCA9555* tca9555) : OutputDriver<>(outputs), tca9555(tca9555){
+
+}
+
+void OutputTCA::performWrite(int port, float value) noexcept{
+	if(!getInversions().contains(port)){
+		CMF_LOG(CMF, Warning, "Output port %d inversion missing", port);
+		return;
+	}
+	tca9555->write(port, (bool) value);
+}
+
+void OutputTCA::performRegister(OutputPinDef output) noexcept{
+	tca9555->pinMode(output.port, TCA9555::PinMode::OUT);
+}
