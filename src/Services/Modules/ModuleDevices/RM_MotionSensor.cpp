fe15b709e8c9e663316e5b2d69246654fcae7bba
Emil Gajšak <emil.gajsak@gmail.com>
2026-05-08 09:58:45 +0200

    ModuleService and drivers fixes

--- src/Services/Modules/ModuleDevices/RM_MotionSensor.cpp
+++ src/Services/Modules/ModuleDevices/RM_MotionSensor.cpp
@@ -1,7 +1,7 @@
 #include "RM_MotionSensor.h"
 
 RM_MotionSensor::RM_MotionSensor(const Modules::BusPins& busPins) : Super(Modules::Type::RM_Motion, busPins){
-	const auto pin = static_cast<gpio_num_t>(busPins.subAddressPins[0].port);
+	const auto pin = static_cast<gpio_num_t>(busPins.subAddressPins[0].inputPort);
 	const gpio_config_t io_conf = {
 			.pin_bit_mask = 1ULL << pin,
 			.mode = GPIO_MODE_INPUT,
@@ -11,15 +11,15 @@ RM_MotionSensor::RM_MotionSensor(const Modules::BusPins& busPins) : Super(Module
 	};
 
 	gpio_intr_enable(pin);
-	gpio_config(&io_conf);
 	gpio_isr_handler_add(pin, isr, this);
+	gpio_config(&io_conf);
 
 	setLEDs(false);
 }
 
 void RM_MotionSensor::setLEDs(bool state){
 	if(pins.subAddressPins[1].outputDriver){
-		pins.subAddressPins[1].outputDriver->write(pins.subAddressPins[1].port, state);
+		pins.subAddressPins[1].outputDriver->write(pins.subAddressPins[1].outputPort, state);
 	}
 }
 
@@ -33,5 +33,5 @@ void RM_MotionSensor::isr(void* arg){
 	}
 
 	RM_MotionSensor* motion = static_cast<RM_MotionSensor*>(arg);
-	motion->active = gpio_get_level(static_cast<gpio_num_t>(motion->pins.subAddressPins[0].port));
+	motion->active = gpio_get_level(static_cast<gpio_num_t>(motion->pins.subAddressPins[0].inputPort));
 }
