ce8003743548e9094b9702ffe2b277485e59dcbb
Emil Gajšak <emil.gajsak@gmail.com>
2026-05-12 19:01:22 +0200

    ModuleService fixed bad single-bus constructor

--- src/Services/Modules/ModuleService.h
+++ src/Services/Modules/ModuleService.h
@@ -31,8 +31,9 @@ public:
 		Insert, Remove
 	};
 
-
-	ModuleService(const Modules::BusPins& busPins) requires (CONFIG_CMF_MODULESERVICE_NUM_BUSES == 1) : ModuleService(std::array{ busPins }){}
+#if CONFIG_CMF_MODULESERVICE_NUM_BUSES == 1
+	ModuleService(const Modules::BusPins& busPins) : ModuleService(std::array{ busPins }){}
+#endif
 
 	ModuleService(std::array<Modules::BusPins, CONFIG_CMF_MODULESERVICE_NUM_BUSES> busPins) :
 			Super(CONFIG_CMF_MODULESERVICE_TICK_INTERVAL / portTICK_PERIOD_MS, CONFIG_CMF_MODULESERVICE_STACK_SIZE, CONFIG_CMF_MODULESERVICE_THREAD_PRIORITY, CONFIG_CMF_MODULESERVICE_CPU_CORE), busPins(std::move(busPins)){
