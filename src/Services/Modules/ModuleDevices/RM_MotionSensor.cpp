9db90e85fd981512a7e24f12128b04ca21d96cb8
Emil Gajšak <emil.gajsak@gmail.com>
2026-05-12 16:51:21 +0200

    Module events  (#44)
    * ModuleService numBus moved to KConfig, module-specific events
    
    * ModuleService events, template removed, BaseBoard ISR registration order fixed
    
    * Code cleanup

--- src/Services/Modules/ModuleDevices/RM_MotionSensor.cpp
+++ src/Services/Modules/ModuleDevices/RM_MotionSensor.cpp
@@ -1,13 +1,48 @@
 #include "RM_MotionSensor.h"
+#include "Memory/ObjectMemory.h"
+#include "Services/Modules/ModuleService.h"
+
+DEFINE_LOG(RM_MotionSensor);
 
 RM_MotionSensor::RM_MotionSensor(const Modules::BusPins& busPins) : Super(Modules::Type::RM_Motion, busPins){
-	const auto pin = static_cast<gpio_num_t>(busPins.subAddressPins[0].inputPort);
+	pin = static_cast<gpio_num_t>(busPins.subAddressPins[0].inputPort);
+	active = gpio_get_level(pin);
+
+	sem = xSemaphoreCreateBinary();
+	thread = newObject<Threaded>(this, [this]() {
+		if(!debouncing){
+			xSemaphoreTake(sem, portMAX_DELAY);
+			debouncing = true;
+			return;
+		}
+
+		if(xSemaphoreTake(sem, pdMS_TO_TICKS(DebounceMs)) == pdTRUE){
+			CMF_LOG(RM_MotionSensor, Info, "ignored debounce");
+			return;
+		}
+
+		debouncing = false;
+		bool newActive = gpio_get_level(pin);
+		if(newActive == active.load()) return;
+
+		active = newActive;
+
+		ModuleService* parent = cast<ModuleService>(getOwner());
+		if(parent == nullptr){
+			CMF_LOG(RM_MotionSensor, Error, "Parent isn't ModuleService!");
+			return;
+		}
+
+		parent->OnRM_Motion.broadcast(active.load());
+	}, "RM_MotionSensor", 0, 3 * 1024);
+
+
 	const gpio_config_t io_conf = {
-			.pin_bit_mask = 1ULL << pin,
-			.mode = GPIO_MODE_INPUT,
-			.pull_up_en = GPIO_PULLUP_DISABLE,
-			.pull_down_en = GPIO_PULLDOWN_ENABLE,
-			.intr_type = GPIO_INTR_ANYEDGE
+		.pin_bit_mask = 1ULL << pin,
+		.mode = GPIO_MODE_INPUT,
+		.pull_up_en = GPIO_PULLUP_DISABLE,
+		.pull_down_en = GPIO_PULLDOWN_ENABLE,
+		.intr_type = GPIO_INTR_ANYEDGE
 	};
 
 	gpio_intr_enable(pin);
@@ -15,6 +50,17 @@ RM_MotionSensor::RM_MotionSensor(const Modules::BusPins& busPins) : Super(Module
 	gpio_config(&io_conf);
 
 	setLEDs(false);
+}
+
+RM_MotionSensor::~RM_MotionSensor() noexcept{
+	gpio_isr_handler_remove(pin);
+
+	thread->stop(0);
+	xSemaphoreGive(sem);
+	thread->stop();
+	delete thread.get();
+
+	vSemaphoreDelete(sem);
 }
 
 void RM_MotionSensor::setLEDs(bool state){
@@ -27,11 +73,12 @@ bool RM_MotionSensor::getState() const{
 	return active;
 }
 
-void RM_MotionSensor::isr(void* arg){
+void IRAM_ATTR RM_MotionSensor::isr(void* arg){
 	if(arg == nullptr){
 		return;
 	}
 
 	RM_MotionSensor* motion = static_cast<RM_MotionSensor*>(arg);
-	motion->active = gpio_get_level(static_cast<gpio_num_t>(motion->pins.subAddressPins[0].inputPort));
+	BaseType_t higherPrioTask = pdFALSE;
+	xSemaphoreGiveFromISR(motion->sem, &higherPrioTask);
 }
