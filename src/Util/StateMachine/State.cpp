867112c43134f6425425b93324bbdd257a74482e
Fr0zen3k <jan.kresic@circuitmess.com>
2026-05-15 12:40:53 +0200

    Added additional checks for state transitioning

--- src/Util/StateMachine/State.cpp
+++ src/Util/StateMachine/State.cpp
@@ -20,10 +20,20 @@ StateMachine* State::getStateMachine() const noexcept{
 	return cast<StateMachine>(getOwner());
 }
 
-void State::transitionTo(const Class* state) const {
+void State::transitionTo(const Class* state) const{
+	if(state == nullptr){
+		CMF_LOG(State, LogLevel::Error, "Transition but given state class parameter is nullptr.");
+		return;
+	}
+
+	if(!state->isA(staticClass())){
+		CMF_LOG(State, LogLevel::Error, "Transition but given state class parameter is not of Class type.");
+		return;
+	}
+
 	StateMachine* stateMachine = getStateMachine();
 	if(stateMachine == nullptr){
-		CMF_LOG(State, LogLevel::Warning, "Transition but StateMachine is nullptr");
+		CMF_LOG(State, LogLevel::Error, "Transition but StateMachine is nullptr");
 		return;
 	}
 
