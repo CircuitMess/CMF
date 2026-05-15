c3c1694bcc86ad11717b2b7eb7dc4f37a5af657c
Jan Kresic <46379092+Fr0zen3k@users.noreply.github.com>
2026-05-15 17:59:09 +0200

    Changes to state machine and state ticking synchronization (#41)
    * Changes to state machine and state ticking synchronization, state machine tick wait bypass added
    
    * Added additional checks for state transitioning
    
    * Changed state and state machine API to allow states to change StateMachine ticking interval dynamically at runtime
    
    ---------
    
    Co-authored-by: Emil Gajšak <emil.gajsak@gmail.com>

--- src/Util/StateMachine/State.cpp
+++ src/Util/StateMachine/State.cpp
@@ -1,9 +1,8 @@
 #include "State.h"
 #include "StateMachine.h"
+#include "Log/Log.h"
 
-const Class* State::transitionTo() const noexcept{
-	return next;
-}
+DEFINE_LOG(State);
 
 void State::onTransitionFrom(const Class* previous) noexcept{
 
@@ -13,14 +12,30 @@ void State::onTransitionTo(const Class* next) noexcept{
 
 }
 
+int64_t State::getDynamicTickInterval() const noexcept{
+	return -1;
+}
+
 StateMachine* State::getStateMachine() const noexcept{
 	return cast<StateMachine>(getOwner());
 }
 
-void State::setNextState(const Class* state) noexcept {
-	if(!state->isA(staticClass())){
+void State::transitionTo(const Class* state) const{
+	if(state == nullptr){
+		CMF_LOG(State, LogLevel::Error, "Transition but given state class parameter is nullptr.");
 		return;
 	}
 
-	next = state;
+	if(!state->isA(staticClass())){
+		CMF_LOG(State, LogLevel::Error, "Transition but given state class parameter is not of Class type.");
+		return;
+	}
+
+	StateMachine* stateMachine = getStateMachine();
+	if(stateMachine == nullptr){
+		CMF_LOG(State, LogLevel::Error, "Transition but StateMachine is nullptr");
+		return;
+	}
+
+	stateMachine->transitionTo(state);
 }
